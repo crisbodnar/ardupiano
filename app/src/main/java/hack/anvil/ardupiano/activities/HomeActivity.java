a22046e54b8803802d384e90a8f836a518dbfd1d
Igor Wodiany <igor.wodiany@gmail.com>
2016-05-07 22:08:22 +0100

    Oprimaze imports
    Remove unused imports from HomeActivity

--- app/src/main/java/hack/anvil/ardupiano/activities/HomeActivity.java
+++ app/src/main/java/hack/anvil/ardupiano/activities/HomeActivity.java
@@ -4,9 +4,8 @@ import android.bluetooth.BluetoothAdapter;
 import android.bluetooth.BluetoothDevice;
 import android.bluetooth.BluetoothSocket;
 import android.content.Intent;
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
-import android.util.Log;
+import android.support.v7.app.AppCompatActivity;
 import android.widget.Toast;
 
 import java.io.IOException;
